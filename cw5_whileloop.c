3fef76c6c4ee99e798ae50531a0137eaf821d4d9
ajpinazamora <ajpinazamora@gmail.com>
2025-10-30 15:05:33 -0500

    While loop completed

---
+++ cw5_whileloop.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+        int num, units;
+        double  tp,  rate;
+
+        printf("Enter number of costumers: ");
+        scanf("%d", &num);
+	int i = 0;
+
+        while (i < num){
+                printf("Enter number of units comsumed by costumer: ");
+                scanf("%d", &units);
+
+        if ( units <= 100){
+                rate = units*0.50;
+                tp = rate+20.00;
+        }else if ( units <= 300){
+                rate = units*0.75;
+                tp = rate+20;
+        }else if ( units <= 600){
+                rate = units*1.20;
+                tp = rate+20;
+                if (tp > 500){
+                        tp = tp*1.10;
+                }
+        }else {
+                rate = units*1.50;
+                tp = rate+20;
+                  if (tp > 500){
+                        tp = tp*1.10;
+                }
+	}
+        printf("Total bill for customer %d: $%.2f\n", i+1, tp);
+	i++;
+}
+        return 0;
+}   
