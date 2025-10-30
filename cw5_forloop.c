ed7409b3720d932304067a9819dcc7c377cf5d61
ajpinazamora <ajpinazamora@gmail.com>
2025-10-30 14:58:00 -0500

    Complete For loop

--- cw5_forloop.c
+++ cw5_forloop.c
@@ -8,34 +8,30 @@ int main(){
 	scanf("%d", &num);
 
 	for (int i = 0; i < num; i++){
-		printf("Enter number of electricity units comsumed: ");
+		printf("Enter number of units comsumed by costumer: ");
        		scanf("%d", &units);
 
 	if ( units <= 100){
 		rate = units*0.50;
 		tp = rate+20.00;
-		printf("Total bill: %.2f\n", tp);
-	}else if ( 100 < units <= 300){
+	}else if ( units <= 300){
 		rate = units*0.75;
 		tp = rate+20;
-		printf("Total bill: %.2f\n", tp);
-	}else if ( 300 < units <= 600){
+	}else if ( units <= 600){
 		rate = units*1.20;
 		tp = rate+20;
 		if (tp > 500){
-			tp *= tp*.10;
+			tp = tp*1.10;
 		}
-		printf("Total bill: %.2f\n", tp);
 	}else {
 		rate = units*1.50;
 		tp = rate+20;
 		  if (tp > 500){
-                        tp *= tp*.10;
+                        tp = tp*1.10;
                 }
-
-		printf("Total bill: %.2f\n", tp);
 	}
-}
+	printf("Total bill for customer %d: $%.2f\n", i+1, tp);
 
+}
 	return 0;
 }
